2e210732b9b53fc42a416a44f0d6bebd52fa6c4e
Patrici0 <patriciov@gmail.com>
2019-05-03 13:34:49 -0300

    updated 00 app code. app/js/coming-soon.js and modified jpg extension by png

--- 00-Application/app/js/coming-soon.js
+++ 00-Application/app/js/coming-soon.js
@@ -4,9 +4,9 @@
   // Vide - Video Background Settings
   $('body').vide({
     mp4: "mp4/bg.mp4",
-    poster: "img/bg-mobile-fallback.jpg"
+    poster: "img/bg-mobile-fallback.png"
   }, {
-    posterType: 'jpg'
+    posterType: 'png'
   });
 
 })(jQuery); // End of use strict
