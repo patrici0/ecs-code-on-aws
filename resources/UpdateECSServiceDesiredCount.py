84fd2439b09fc77084da1ec5e10030050837324c
patvaz <patvaz@amazon.com>
2020-06-12 19:31:58 -0300

    updated sample lambda to updated ECS service desired count for tasks

--- resources/UpdateECSServiceDesiredCount.py
+++ resources/UpdateECSServiceDesiredCount.py
@@ -17,31 +17,38 @@ def lambda_handler(event, context):
     logger.info('got context: {}'.format(context))
 
     # Parameters validation
+    UserParameters = event['CodePipeline.job']['data']['actionConfiguration']['configuration']['UserParameters']
+    parameters = {
+        'ecs_cluster': UserParameters.split()[0],
+        'ecs_service': UserParameters.split()[1],
+        'ecs_service_desired_count': UserParameters.split()[2]
+    }
+
     ecs_cluster = ""
     
-    if ('ecs_cluster' not in event):
-        logger.error("ERROR: Unexpected error: Could not find event['ecs_cluster']")
+    if ('ecs_cluster' not in parameters):
+        logger.error("ERROR: Unexpected error: Could not find parameters['ecs_cluster']")
         sys.exit()
     else:
-        ecs_cluster = event['ecs_cluster']
+        ecs_cluster = parameters['ecs_cluster']
         logger.info(ecs_cluster)
 
     ecs_service = ""
     
-    if ('ecs_service' not in event):
-        logger.error("ERROR: Unexpected error: Could not find event['ecs_service']")
+    if ('ecs_service' not in parameters):
+        logger.error("ERROR: Unexpected error: Could not find parameters['ecs_service']")
         sys.exit()
     else:
-        ecs_service = event['ecs_service']
+        ecs_service = parameters['ecs_service']
         logger.info(ecs_service)
 
     ecs_service_desired_count = ""
     
-    if ('ecs_service_desired_count' not in event):
-        logger.error("ERROR: Unexpected error: Could not find event['ecs_service_desired_count']")
+    if ('ecs_service_desired_count' not in parameters):
+        logger.error("ERROR: Unexpected error: Could not find parameters['ecs_service_desired_count']")
         sys.exit()
     else:
-        ecs_service_desired_count = event['ecs_service_desired_count']
+        ecs_service_desired_count = int(parameters['ecs_service_desired_count'])
         logger.info(ecs_service_desired_count)
     
     # Set up ECS Client
